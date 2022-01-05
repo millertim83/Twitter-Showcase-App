242aa175165fa590b928bcaa71e5e9edd42be5b7
millertim83 <80060826+millertim83@users.noreply.github.com>
2022-01-05 13:18:39 -0500

    updated TweetCard

--- client/src/Components/TweetCard.js
+++ client/src/Components/TweetCard.js
@@ -15,16 +15,54 @@ export default function TweetCard({ show, onClose, tweetResults, userInfo, tweet
     return (
         tweetResults && (
             <div>
-                <div className="container border">
-                    <p>{tweet.text}</p>  
-                    <p>{userInfo.username}</p>
+                <div id="tweet-modal" className="container">
+                    <div className="row">
+                        <div className="col-2">
+                            <img id="profile-pic"
+                            className="rounded-circle responsive-img"
+                            src={userInfo.profile_image_url}
+                            alt={userInfo.name}>
+                            </img>
+                        </div>
+                        <div id="name" className="col-3">
+                            <p>{userInfo.name} {userInfo.verified === true ? <img id="check" src={check}/> : null}</p>
+                        </div>  
+                        <div className="col-3">
+                            <p id="username">{ `@${userInfo.username}` }</p>
+                        </div>
+                        <div className="col-4">
+                            <p id="date">{tweet.created_at}</p>
+                        </div>
+    
+                    </div>
+                    <div className="row">
+                        <div className="col-2">
+
+                        </div>
+                        <div className="col-10 text-start">
+                            <p className="text-sm-start">{tweet.text}</p>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-6">
+                            <p>{<img id="heart" src={heart} alt="likes:" />} {tweet.public_metrics.like_count}</p>
+                        </div>
+                        <div className="col-6">
+                            <p>{<img id="retweet" src={retweet} alt="retweets:" />} {tweet.public_metrics.retweet_count}</p>
+                        </div>
+                    </div>
                 </div>
-                
-                <button onClick={onClose}>Close</button>  
+                    <button onClick={onClose}>Close</button>  
             </div> 
         )  
     );
 }
+/*
+<div className="container border">
+                    <p>{tweet.text}</p>  
+                    <p>{userInfo.username}</p>
+                </div>
+                */
 
 /*
 <div id="tweet-modal" className="container">

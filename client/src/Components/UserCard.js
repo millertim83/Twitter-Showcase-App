654d02924c17e44ad1d3707c7b6260119be71d0c
millertim83 <80060826+millertim83@users.noreply.github.com>
2022-01-13 13:51:39 -0500

    added function to format date for Favorite users

--- client/src/Components/UserCard.js
+++ client/src/Components/UserCard.js
@@ -6,10 +6,57 @@ import calendar from "../images/calendar.png";
 
 export default function UserCard({userInfo})  {
     let userData = userInfo;
-    
+
+    function joinedTwitterDate(dateString) {
+        let dateArray = dateString.split("-");
+        let year = dateArray[0];
+        let month = dateArray[1];
+        switch (month) {
+            case "01": 
+                month = "Jan";
+            break;
+            case "02": 
+                month = "Feb";
+            break;
+            case "03": 
+                month = "March";
+            break;
+            case "04": 
+                month = "April";
+            break;
+            case "05": 
+                month = "May";
+            break;
+            case "06": 
+                month = "June";
+            break;
+            case "07": 
+                month = "July";
+            break;
+            case "08": 
+                month = "Aug";
+            break;
+            case "09": 
+                month = "Sep";
+            break;
+            case "10": 
+                month = "Oct";
+            break;
+            case "11": 
+                month = "Nov";
+            break;
+            case "12": 
+                month = "Dec";
+        }
+        
+
+        return(`${month} ${year}`) ;
+    } 
+
+
     return (
         userInfo && (
-            <div id="user-card" className="container bg-white h-20 m-3 p-3">
+            <div id="user-card" className="container bg-white m-3 p-3">
                 <div className="row">
                         <div className="col-2">
                             <img id="profile-pic" 
@@ -19,21 +66,32 @@ export default function UserCard({userInfo})  {
                             </img>
                         </div>
                         
-                        <div className="col-5 text-center align-text-bottom">  
+                        <div className="col-5 text-center pt-2">  
                             <p id="name">{userData.data.name} {userData.data.verified === true ? <img id="check" src={check}/> : null} </p>  
                         </div> 
-                        <div className="col-5 text-center align-text-bottom">
+                        <div className="col-5 text-center pt-2">
                             <p id="username" className="text-secondary">{`@${userData.data.username}`}</p>
                         </div>
                 </div>
-                <div id="description" className="row">
-                    <p id="user-description">{userData.data.description}</p>
+                <div  className="row">
+                    <div className="col-3">
+
+                    </div>
+                    <div id="description" className="col-8">
+                        <p id="user-description">{userData.data.description}</p>
+                    </div>
                 </div>
                 <div className="row">
-                    <div className="col-12 text-center">
-                        <span id="location">{userData.data.location ? <img id="pin" src={pin} /> : null } {userData.data.location} </span>
-                        <p id="join">{<img id="calendar" src={calendar}/>} {userData.data.created_at}</p>
+                    <div className="col-2">
+
                     </div>
+                    <div className="col-5 text-center">
+                        <p id="location">{userData.data.location ? <img id="pin" src={pin} /> : null } {userData.data.location} </p>
+                    </div>
+                    <div className="col-5">
+                        <p id="join">{<img id="calendar" src={calendar}/>} {`Joined ${joinedTwitterDate(userData.data.created_at)}`}</p>
+                    </div>
+
                 </div>
                 <div className="row justify-content-around">
                     <div id="followers" className="col-5">

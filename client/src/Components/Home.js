2c6a7014b98f4fe866a1ad042f93e717a80357ed
millertim83 <80060826+millertim83@users.noreply.github.com>
2022-01-21 19:36:44 -0500

    new color scheme, removed bg image

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,16 +1,17 @@
 import React from "react";
 import "./Home.css";
-import frustrated from "../images/angry.png";
+import frustrated from "../images/frustrated.jpg";
 
 function Home() {
     return(
         <div className = "Home">
             <div className = "body mb-3">
-                <div className="container">
-                <h1>Serenity Now</h1>
-                <h4>A Twitter Portal</h4>
-            </div>
-                
+                <div id = "header">
+                    <div   className="container-lg">
+                        <h1>Serenity Now</h1>
+                        <h4>A Twitter Portal</h4>
+                    </div>
+                </div>
                 <div className="container-sm mt-4">
                     <div className="row">
                         <div className="col-4">
@@ -20,8 +21,8 @@ function Home() {
                                 alt="Frustrated guy">
                             </img>
                         </div>
-                        <div className="col-6">
-                            <p>If you feel like this guy, head to the Favorites page to see some Tweets that may add some calm 
+                        <div className="col-6 pt-3">
+                            <p className="mt-3 pt-3">If you feel like this guy, head to the Favorites page to see some Tweets that may add some calm 
                                 to your life. Otherwise, go to the Search page to look up your favorite users or topics.</p>
                         </div>
                     </div>
